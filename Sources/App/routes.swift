b42d8556574ed064b38627aaef994083212b5b71
Steven Roebert <steven@roebert.nl>
2023-02-01 10:04:47 +0100

    Removed test endpoint

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,8 +4,4 @@ func routes(_ app: Application) throws {
     app.get { _ in
         HTTPStatus.noContent
     }
-    
-    app.get("test") { request in
-        try await request.teslaAPI.getVehicles()
-    }
 }
